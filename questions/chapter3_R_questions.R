c784e8d76d9f481f27f1f4ca2bec3f4e75006d12
Samuel Perini <samuelperini@va185117.shef.ac.uk>
2019-08-22 17:34:42 +0200

    question chapter 3

---
+++ questions/chapter3_R_questions.R
@@ -0,0 +1,39 @@
+## Study questions for Chapter 3 computer lab session ##
+
+# Name:
+# Date:
+
+### Discussion questions
+
+#1.	The Hardy-Weinberg model hardly applies to any natural population. 
+#   Why is it nevertheless useful?
+
+#2. In a small population, genetic drift will lead to a reduction in the frequency
+#   of heterozygotes relative the Hardy-Weinberg expectation - is this true or false?
+#   Discuss why.
+
+## 3. Why is it that when an allele goes to fixation in a population, there are no
+# heterozygotes but there is also no deviation from the Hardy-Weinberg expectation?
+
+### R coding questions
+
+## 4. Write a simple R function called square that will return the square of any numeric
+##    variable it is given.
+
+## 5.	You genotype a species of grasshoppers along a north south transect across the European Alps.
+##    Near Munich, Germany, north of the Alps you sample 120 individuals; near Innsbruck, Austria, 
+##    within the Alps you sample 122 individuals; near Verona, Italy, south of the Alps you sample 
+##   118 individuals. You find the following number of each genotype.
+##
+##   Munich - 6 (A1A1), 33 (A1A2), 81 (A2A2)
+##   Innsbruck  - 20 (A1A1), 59 (A1A2), 43 (A2A2)
+##   Verona - 65 (A1A1), 39 (A1A2), 14 (A2A2)
+##
+##   Using the R code we learnt during the tutorial, calculate the allele frequencies in each population.
+##   Then test whether there is a deviation from Hardy-Weinberg Equilibrium in each of them.
+
+## 6. Using the functions we wrote during the tutorial, simulate drift over 2000 generations for a population
+##    of 100 individuals. How does altering p from 0.3, 0.5 and 0.9 alter the outcome of the simulations? Which
+##    is more likely to go to fixation?
+
+  
